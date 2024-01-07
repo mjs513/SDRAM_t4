160a5d0a39912721b4b060d0aa973937f8aeaee2
Mike S <CyberMerln@gmail.com>
2024-01-07 10:26:10 -0500

    Added four test cases

---
+++ examples/SDRAM_MALLOC/SDRAM_MALLOC.ino
@@ -0,0 +1,75 @@
+#include <SDRAM_t4.h>
+
+//constructor for 
+SDRAM_t4 sdram;
+
+/******************************************************************************************
+ * Example of using Malloc for SDRAM taken from geeksforgeeks
+ * https://www.geeksforgeeks.org/dynamic-memory-allocation-in-c-using-malloc-calloc-free-and-realloc/?ref=lbp
+ *
+ * "... used to dynamically allocate a single large block of memory with the specified size. 
+ * It returns a pointer of type void which can be cast into a pointer of any form. It doesn’t 
+ * Initialize memory at execution time so that it has initialized each block with the default 
+ * garbage value initially."
+ *
+ *********************************************************************************************/
+ 
+void setup()
+{
+    while(!Serial && millis() < 5000){}
+    Serial.begin(115200);
+
+    if(CrashReport) {
+      Serial.print(CrashReport);
+      while(1);
+    }
+ 
+    // sdram.begin initializes the available SDRAM Module
+    // begin defaults to 32mb but you can specify the size
+    // from begin
+    // begin(uint16 external_memory_size) where size is in
+    // MB
+    if(!sdram.begin()) {
+      Serial.printf("SDRAM Init Failed!!!\n");
+      while(1);
+    };
+
+    // This pointer will hold the
+    // base address of the block created
+    int* ptr;
+    int n, i;
+ 
+    // Get the number of elements for the array
+    n = 24;
+    printf("Entered number of elements: %d\n", n);
+ 
+    // Dynamically allocate memory using sdram_malloc
+    // format: (cast-type*) sdram_malloc(byte-size)
+    ptr = (int*)sdram_malloc(n * sizeof(int));
+ 
+    // Check if the memory has been successfully
+    // allocated by malloc or not
+    if (ptr == NULL) {
+        Serial.printf("Memory not allocated.\n");
+        exit(0);
+    }
+    else {
+ 
+        // Memory has been successfully allocated
+        printf("Memory successfully allocated using malloc.\n");
+ 
+        // Get the elements of the array
+        for (i = 0; i < n; ++i) {
+            ptr[i] = i + 1;
+        }
+ 
+        // Print the elements of the array
+        Serial.printf("The elements of the array are: ");
+        for (i = 0; i < n; ++i) {
+            Serial.printf("%d, %x \n", ptr[i], &ptr[i]);
+        }
+    }
+
+}
+
+void loop(){}
---
+++ examples/SDRAM_REALLOC/SDRAM_REALLOC.ino
@@ -0,0 +1,96 @@
+#include "SDRAM_t4.h"
+
+//constructor for 
+SDRAM_t4 sdram;
+
+/******************************************************************************************
+ * Example of using Malloc for SDRAM taken from geeksforgeeks
+ * https://www.geeksforgeeks.org/dynamic-memory-allocation-in-c-using-malloc-calloc-free-and-realloc/?ref=lbp
+ *
+ * "... used to dynamically change the memory allocation of a previously allocated memory"
+ *
+ *********************************************************************************************/
+
+void setup() {
+    while(!Serial && millis() < 5000);
+    Serial.begin(115200);
+
+    if(CrashReport) {
+      Serial.println(CrashReport);
+      while(1) ;
+    }
+
+    // sdram.begin initializes the available SDRAM Module
+    // begin defaults to 32mb but you can specify the size
+    // from begin
+    // begin(uint16 external_memory_size) where size is in
+    // MB
+    if(!sdram.begin()) {
+      Serial.println("SDRAM FAILED TO INITIALIZE.....");
+      //while(1);
+    }
+    
+	// This pointer will hold the
+	// base address of the block created
+	int* ptr;
+	int n, i;
+
+	// Get the number of elements for the array
+	n = 5;
+	Serial.printf("Enter number of elements: %d\n", n);
+
+	// Dynamically allocate memory using calloc()
+	ptr = (int*)sdram_calloc(n, sizeof(int));
+
+	// Check if the memory has been successfully
+	// allocated by malloc or not
+	if (ptr == NULL) {
+		Serial.printf("Memory not allocated.\n");
+		exit(0);
+	}
+	else {
+
+		// Memory has been successfully allocated
+		Serial.printf("Memory successfully allocated using calloc.\n");
+
+		// Get the elements of the array
+		for (i = 0; i < n; ++i) {
+			ptr[i] = i + 1;
+		}
+
+		// Print the elements of the array
+		Serial.printf("The elements of the array are: ");
+		for (i = 0; i < n; ++i) {
+      Serial.printf("%d, ", ptr[i]);
+		}
+
+		// Get the new size for the array
+		n = 10;
+		Serial.printf("\n\nEnter the new size of the array: %d\n", n);
+
+		// Dynamically re-allocate memory using realloc()
+    // sdram_realloc(ptr, newSize)
+		ptr = (int*)sdram_realloc(ptr, n * sizeof(int));
+
+		// Memory has been successfully allocated
+		Serial.printf("Memory successfully re-allocated using realloc.\n");
+
+		// Get the new elements of the array
+		for (i = 5; i < n; ++i) {
+			ptr[i] = i + 1;
+		}
+
+		// Print the elements of the array
+		Serial.printf("The elements of the array are: ");
+		for (i = 0; i < n; ++i) {
+        Serial.printf("%d, ", ptr[i]);
+		}
+
+		sdram_free(ptr);
+	}
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
