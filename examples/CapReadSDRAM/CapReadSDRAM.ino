54a15827b13e7984591b0c18a6bdd472b7e48f29
Defragster <Defragster@users.noreply.github.com>
2024-01-27 19:30:21 -0800

    spare ) gone

--- examples/CapReadSDRAM/CapReadSDRAM.ino
+++ examples/CapReadSDRAM/CapReadSDRAM.ino
@@ -80,7 +80,7 @@ void loop() {
     testmsec = millis() - testmsec;
     Serial.printf("\nTest result: %u read errors\n\nExtra info: ran for %.2f seconds)\n", totErrs, testmsec / 1000.0);
 #ifdef USB_DUAL_SERIAL
-    SerialUSB1.printf("\nDone with total errors found %u\t(time %.2f secs)\n", totErrs, testmsec / 1000.0);
+    SerialUSB1.printf("\nDone with total errors found %u\t(time %.2f secs\n", totErrs, testmsec / 1000.0);
 #endif
   }
   digitalWrite(13, HIGH);
