3542c14e1b923ed1d986990866b5e77ea35799a1
Mike S <CyberMerln@gmail.com>
2024-01-29 07:25:06 -0500

    added getFrequency method

--- SDRAM_t4.h
+++ SDRAM_t4.h
@@ -14,15 +14,15 @@ void *sdram_realloc(void *ptr, size_t size);
 
 class SDRAM_t4 {
 public:
-    constexpr SDRAM_t4() {  };
-    static bool begin(uint8_t external_sdram_size = 32, uint16_t clock = 166, uint8_t useDQS = 0);
-    
+    SDRAM_t4() {  };
+    bool begin(uint8_t external_sdram_size = 32, uint16_t clock = 166, uint8_t useDQS = 1);
+    inline float getFrequency() { return frequency; }
 private:
     static unsigned int ns_to_clocks(float ns, float freq);
     static void configure_sdram_pins();
     static bool SendIPCommand(uint32_t address, uint16_t command, uint32_t write, uint32_t *read);
     static bool IPCommandComplete();
     static bool check_fixed_pattern(uint32_t pattern);
-       
+    float frequency;   
 };
 #endif
